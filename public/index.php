5079013d7f0f129741b7651ef3f4ed7af794053b
tanguilb <tangui.lebourdonnec@gmail.com>
2016-10-06 20:53:33 +0200

    commit initial

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tangui
+ * Date: 06/10/16
+ * Time: 17:46
+ */
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>COncours des plus belles chaussettes</title>
+            <meta charset="utf-8" name="viewport" >
+            <meta name="Concours des plus belles chaussettes de la wild code school" >
+            <link rel="stylesheet" src="css/style.css" >
+        </head>
+        <body>
+
+        </body>
+</html>
