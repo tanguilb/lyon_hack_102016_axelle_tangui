056761cf755afe9e97a156d94177101a49a9ec8f
Axelle-slv <axelle.silva@gmail.com>
2016-10-06 22:05:37 +0200

    Creation of the html structure for the gallery

---
+++ public/gallery.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Galerie Photos | Election des meilleures chaussettes</title>
+    <meta name="description" content="Bienvenue dans notre galerie photos pour l'élection des meilleures chaussettes de l'inauguration de la Wild Code School.">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <h2>Quelles sont les trois paires de chaussettes que vous préférez ?</h2>
+        <hr>
+    </div>
+    <div class="row">
+        <span class="col-lg-3 col-sm-12 picture">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+
+        <span class="col-lg-3 col-sm-12">
+            <img class="img-thumbnail" src="//placehold.it/200x200">
+        </span>
+    </div>
+</div>
+
+</body>
+</html>
