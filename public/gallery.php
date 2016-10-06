a33170896c7b17d855b34b63b273ac415e2b48b8
Axelle-slv <axelle.silva@gmail.com>
2016-10-06 22:37:52 +0200

    Creation of the style.css file for the gallery of the best socks

--- public/gallery.php
+++ public/gallery.php
@@ -4,9 +4,15 @@
     <meta charset="utf-8">
     <title>Galerie Photos | Election des meilleures chaussettes</title>
     <meta name="description" content="Bienvenue dans notre galerie photos pour l'élection des meilleures chaussettes de l'inauguration de la Wild Code School.">
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-</head>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Oswald:300,400" rel="stylesheet">
+        <!--Bootstrap CDN for CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!--Our stylesheet-->
+        <link rel="stylesheet" href="../public/css/style.css">
+        <!--Our script-->
+        <script src="script.js"></script>
+    </head>
 <body>
 
 <div class="container">
