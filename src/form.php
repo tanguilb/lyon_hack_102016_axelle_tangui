86636da990366ef64e82e076b373adf33c57078c
tanguilb <tangui.lebourdonnec@gmail.com>
2016-10-06 22:54:57 +0200

    function to upload files on server

--- src/form.php
+++ src/form.php
@@ -8,7 +8,49 @@
 
 include 'bdd.php';
 
-$req = "INSERT INTO guest(path_picture, guest_name) VALUES ('$POST[picture], $POST[guest_name]')";
 
+
+// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+
+if (isset($_FILES['picture']) AND $_FILES['picture']['error'] == 0)
+
+{
+
+    // Testons si le fichier n'est pas trop gros
+
+    if ($_FILES['picture']['size'] <= 1000000)
+
+    {
+
+        // Testons si l'extension est autorisée
+
+        $infosfichier = pathinfo($_FILES['picture']['name']);
+
+        $extension_upload = $infosfichier['extension'];
+
+        $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+
+        if (in_array($extension_upload, $extensions_autorisees))
+
+        {
+
+            // On peut valider le fichier et le stocker définitivement
+
+            move_uploaded_file($_FILES['picture']['tmp_name'], '../public/img/' . basename($_FILES['picture']['name']));
+
+            echo "L'envoi a bien été effectué !";
+
+        }
+
+    }
+
+}
+
+$req = 'INSERT INTO guest (path_picture, guest_name) VALUES ("../public/img/$_FILES[\'picture\'][\'name\']"," $_POST[guest_name]")'
 $result = execSql(getConnexion(), $req);
+
 header('location: ../public/index.php');
+
+?>
+
+
